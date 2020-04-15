c6bceeabb61370c05752c4440da8d5b57d23a48e
leye20 <leyeagbo@gmail.com>
2020-04-15 11:57:01 -0600

    store added

--- src/store/products/reducers.tsx
+++ src/store/products/reducers.tsx
@@ -10,28 +10,29 @@ export interface IProduct {
 
 // Define the state.
 export interface IProductState {
-    products: IProduct[]
+    readonly products: IProduct[]
 }
 
+// Default populated state value (empty array, in this case.)
 const initialProductState: IProductState = {
     products: []
 }
 
-export const productReducer: Reducer<IProductState, ProductActions> =
+export const productReducer: Reducer<IProductState, ProductActions> = 
 ( state = initialProductState, action ) => {
     switch ( action.type )
     {
         case ProductActionTypes.GET_ALL:
             return {
                 ...state, // Spread operator.
-                //products: action.products
+                // products: action.products
             }
         case ProductActionTypes.GET_ONE:
             return {
-                ...state, // spread operator.
+                ...state, // Spread operator.
                 // products: action.product
             }
-            default:
-                return state;
+        default:
+            return state;
     }
 }
